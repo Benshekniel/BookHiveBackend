0be88179bbbb85f7e07e19ab7586f3b7a7e4e723
Benshekniel <benshekniel@gmail.com>
2025-06-12 14:49:31 +0530

    Initial commit

---
+++ BookHive/src/main/java/org/example/bookhive/BookHiveApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHiveApplication.class, args);
+    }
+
+}
