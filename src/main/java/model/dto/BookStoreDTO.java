696953eec70849668b9d7eafdca45539ef69bbbc
JohnManuel <sp.john.manuel737@gmail.com>
2025-07-31 21:38:52 +0530

    created empty commented BookStoreDTO

---
+++ src/main/java/model/dto/BookStoreDTO.java
@@ -0,0 +1,23 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDateTime;
+public class BookStoreDTO {
+
+//    @Data @NoArgsConstructor @AllArgsConstructor
+//    public class RegisterBookStoreDTO {
+//
+//    }
+//    @Data @NoArgsConstructor @AllArgsConstructor
+//    public class UpdateBookStoreDTO {
+//
+//    }
+//    @Data @NoArgsConstructor @AllArgsConstructor
+//    public class ListBookStoreDTO {
+//
+//    }
+}
