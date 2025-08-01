0dd150a1e9e268d7c02cb511d92fed738140c1ee
JohnManuel <sp.john.manuel737@gmail.com>
2025-08-01 19:22:08 +0530

    renamed to BookStoreDTOs since it contains multiple dtos and this makes sense

---
+++ src/main/java/model/dto/BookStoreDTOs.java
@@ -0,0 +1,29 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class BookStoreDTOs {
+
+//    @Data @NoArgsConstructor @AllArgsConstructor
+//    public static class BookDTO {
+//        private Integer storeId;
+//        private String storeName;
+//        private String description;
+//        private String email;
+//        private Map<String, String> businessHours;
+//    }
+    @Data @NoArgsConstructor @AllArgsConstructor
+    public static class RegisterBookStoreDTO {
+
+    }
+    @Data @NoArgsConstructor @AllArgsConstructor
+    public static class UpdateBookStoreDTO {
+
+    }
+    @Data @NoArgsConstructor @AllArgsConstructor
+    public static class ListBookStoreDTO {
+
+    }
+}
