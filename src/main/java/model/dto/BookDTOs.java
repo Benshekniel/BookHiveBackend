5df2c7e1440c5fc13982f3d5128f5002bf340c01
JohnManuel <sp.john.manuel737@gmail.com>
2025-08-03 01:42:48 +0530

    moved BookDTOs to main dto folder

--- src/main/java/model/dto/BookDTOs.java
+++ src/main/java/model/dto/BookDTOs.java
@@ -1,12 +1,8 @@
-package model.dto.BookStore;
-
-import model.entity.Book;
+package model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Map;
 
 public class BookDTOs {
 
