2de2fa03d472ba41beda3690d072ce9d33f06828
John Manuel <sp.john.manuel737@gmail.com>
2025-09-13 03:37:19 +0530

    separate dtos, contrlr, service for all bs stats in one

--- src/main/java/model/dto/BookStore/BSStatDTOs.java
+++ src/main/java/model/dto/BookStore/BSStatDTOs.java
@@ -11,15 +11,38 @@ import java.util.Map;
 public class BSStatDTOs {
 
     @Data @NoArgsConstructor @AllArgsConstructor
+    public static class RegularInventoryStatDTO {
+        private long totalBooks;
+        private long sellInventory;
+        private long lendInventory;
+        private long notSetInventory;
+    }
+
+    @Data @NoArgsConstructor @AllArgsConstructor
+    public static class DonationInventoryStatDTO {
+        private long totalBooks;
+        private long donated;
+        private long donationStock;
+        private long impactScore;
+    }
+
+    @Data @NoArgsConstructor @AllArgsConstructor
     public static class SaleStatDTO {
         private long totalBooks;
         private long activeListings;
+        private long inInventory;
+        private long soldCount;
+
+        private long avgSellPrice;
     }
 
     @Data @NoArgsConstructor @AllArgsConstructor
     public static class LendStatDTO {
         private long totalBooks;
         private long activeListings;
-        private long avgPrice;
+        private long inInventory;
+        private long onLoanCount;
+
+        private long avgLendPrice;
     }
 }
