5fa70bb0aaf22fa4a86bd7a759e3d4d3ba38198e
John Manuel <sp.john.manuel737@gmail.com>
2025-10-19 16:41:44 +0530

    initial commit bs transactions repo, some test ctrl edits, bs donations entity commented

--- src/main/java/model/repo/BookStore/BSTransactionRepo.java
+++ src/main/java/model/repo/BookStore/BSTransactionRepo.java
@@ -1,16 +1,17 @@
-//package model.repo.BookStore;
-//
-//import model.entity.BSTransaction;
-//import org.springframework.stereotype.Repository;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface BSTransactionRepo extends JpaRepository<BSTransaction, Long> {
-//    List<BSTransaction> findByBookStore_StoreId(Integer storeId);
-//    List<BSTransaction> findByUser_UserId(Integer userId);
-//}
-//
+package model.repo.BookStore;
+
+import model.entity.Transaction;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@Repository
+public interface BSTransactionRepo extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByUserId(Long userId);
+
+}
+
